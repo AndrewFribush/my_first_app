cb5846cef20445d8166194ff9d6aa21004e15cd2
Andrew Fribush <andrewfribush@Andrews-MacBook-Pro.local>
2014-10-18 15:46:00 -0400

    making buttons and fields

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 class TweetsController < ApplicationController
 	def new
+		@tweet = Tweet.new
+		@tweets = Tweet.all
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		@tweets = Tweet.all
+		print "the tweet is " + @tweet.inspect.to_s
+		render 'new'
 	end
 
 	def edit
@@ -9,5 +19,9 @@ class TweetsController < ApplicationController
 	end
 
 	def index
+	end
+
+	def tweet_params
+		params.require("tweet").permit("content")
 	end
 end
